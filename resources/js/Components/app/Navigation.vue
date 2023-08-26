410790d56978402e98dfc067085df0d4cc17dea8
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2023-08-26 17:33:30 +0600

    Define Main Part

--- resources/js/Components/app/Navigation.vue
+++ resources/js/Components/app/Navigation.vue
@@ -1,16 +1,15 @@
 <template>
     <div class="min-w-[200px]">
-        <div class="h-[80px] px-3 flex items-center gap-3">
-            <Link href="/">
-            <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800" />
-            </Link>
-            Lara Storage
-        </div>
+        <Link :href="route('myFiles')" class="h-[80px] px-3 flex items-center gap-3">
+        <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800" />
+        Lara Storage
+        </Link>
+
         <div class="px-3">
             <CreateNewDropdown />
 
             <div class="py-3">
-                <NavLink href="#">My Files</NavLink>
+                <NavLink :href="route('myFiles')" :active="$page.url === '/my-files'">My Files</NavLink>
                 <NavLink href="#">Shared with me</NavLink>
                 <NavLink href="#">Shared by me</NavLink>
                 <NavLink href="#">Trash</NavLink>
