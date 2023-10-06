3b1ffe468652d1e011c43c354ac2ea04f57c3e02
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2023-10-06 16:25:47 +0600

    Implement Search

--- resources/js/Components/app/SearchForm.vue
+++ resources/js/Components/app/SearchForm.vue
@@ -1,27 +1,38 @@
 <template>
-    <form class="w-[600px] h-[80px] flex items-center">
-        <TextInput type="text" class="block w-full mr-2" v-model="form.search" autocomplete
+    <div class="w-[600px] h-[80px] flex items-center">
+        <TextInput type="text" class="block w-full mr-2" @keyup.enter.prevent="onSearch" v-model="search" autocomplete
             placeholder="Search for files and folders" />
-    </form>
+    </div>
 </template>
 
 <script setup>
 // Imports
 import TextInput from '@/Components/TextInput.vue';
-import { useForm } from '@inertiajs/vue3';
+import { router } from '@inertiajs/vue3';
+import { ref, onMounted } from 'vue';
+import { emitter, ON_SEARCH } from '@/event-bus.js';
 
 // Uses
-const form = useForm({
-    search: ''
-});
+let params = '';
 
 // Refs
+const search = ref('');
 
 // Props & Emit
 
 // Computed
 
 // Methods
+function onSearch() {
+    params.set('search', search.value)
+    router.get(window.location.pathname + '?' + params.toString());
+}
 
 // Hooks
+onMounted(() => {
+    params = new URLSearchParams(window.location.search);
+    search.value = params.get('search');
+
+    emitter.emit(ON_SEARCH, search.value);
+});
 </script>
--- resources/js/Pages/MyFiles.vue
+++ resources/js/Pages/MyFiles.vue
@@ -47,6 +47,9 @@
                     <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                         Name
                     </th>
+                    <th v-if="search" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                        Path
+                    </th>
                     <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                         Owner
                     </th>
@@ -83,6 +86,9 @@
                         <FileIcon :file="file" />
                         {{ file.name }}
                     </td>
+                    <td v-if="search" class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        {{ file.path }}
+                    </td>
                     <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                         {{ file.owner }}
                     </td>
@@ -118,7 +124,7 @@ import DeleteFilesButton from '@/Components/app/DeleteFilesButton.vue';
 import DownloadFilesButton from '@/Components/app/DownloadFilesButton.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import {httpGet, httpPost} from "@/Helper/http-helper";
-import {showSuccessNotification} from "../event-bus";
+import {emitter, ON_SEARCH, showSuccessNotification} from '@/event-bus.js';
 
 // Uses
 const page = usePage();
@@ -128,6 +134,7 @@ const allSelected = ref(false);
 const onlyFavourites = ref(false);
 const selected = ref({});
 const loadMoreIntersect = ref(null);
+const search = ref('');
 
 const allFiles = ref({
     data: props.files.data,
@@ -232,6 +239,11 @@ onUpdated(() => {
 onMounted(() => {
     params = new URLSearchParams(window.location.search);
     onlyFavourites.value = params.get('favourites') === '1';
+    search.value = params.get('search');
+
+    emitter.on(ON_SEARCH, (value) => {
+        search.value = value;
+    });
 
     const observer = new IntersectionObserver((entries) =>
         entries.forEach(entry => entry.isIntersecting && loadMore()), {
